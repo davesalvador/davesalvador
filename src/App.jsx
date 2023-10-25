381df31af33076ce2485e7dac0ea09f1b8401fcc
davesalvador <101625754+davesalvador@users.noreply.github.com>
2023-10-25 23:35:57 +0800

    finalize

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,13 @@ import Hero from "./components/hero/Hero";
 import Parallax from "./components/parallax/Parallax";
 import Services from "./components/services/Services";
 import Portfolio from "./components/portfolio/Portfolio";
+import Contact from "./components/contact/Contact";
+import Cursor from "./components/cursor/Cursor";
 
 const App = () => {
   return (
     <div>
+      <Cursor />
       <section id="Homepage">
         <NavBar />
         <Hero />
@@ -22,7 +25,9 @@ const App = () => {
         <Parallax type="portfolio" />
       </section>
       <Portfolio />
-      <section id="Contact">{/* <Contact /> */}</section>
+      <section id="Contact">
+        <Contact />
+      </section>
       {/* Framer Motion Crash Course */}
       {/* <Test/>
     <Test/> */}
