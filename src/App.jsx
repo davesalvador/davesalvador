5462579bfa164efb584c256e3698cbc4bdbd7082
davesalvador <101625754+davesalvador@users.noreply.github.com>
2023-10-25 15:01:12 +0800

    Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./app.scss";
 import NavBar from "./components/navbar/NavBar";
+import Hero from "./components/hero/Hero";
 
 const App = () => {
   return (
     <div>
       <section id="Homepage">
         <NavBar />
+        <Hero />
       </section>
       <section id="Services">Parallax</section>
       <section>Services</section>
