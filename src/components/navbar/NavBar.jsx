4519aae35eb8212429d5fec25058050743f6e733
davesalvador <101625754+davesalvador@users.noreply.github.com>
2023-10-25 13:48:52 +0800

    sidebar
    animated sidebar

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,20 +1,35 @@
 import React from "react";
 import "./navbar.scss";
+import { motion } from "framer-motion";
+import SideBar from "../sidebar/SideBar";
 
 const NavBar = () => {
   return (
     <div className="navbar">
       {/* Sidebar */}
+      <SideBar />
       <div className="wrapper">
-        <span>
-          const {"<"} Dave {"/>"}
-        </span>
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          <span>
+            const {"<"} Dave {"/>"}
+          </span>
+        </motion.span>
         <div className="social">
-          <a href="">
+          <a href="#">
             <img src="/facebook.png" alt="" />
+          </a>
+          <a href="#">
             <img src="/instagram.png" alt="" />
-            <img src="/x.png" alt="" />
-            <img src="/github.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/youtube.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/dribbble.png" alt="" />
           </a>
         </div>
       </div>
